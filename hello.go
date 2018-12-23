9dfabbbde7d0c5ebfff3206829bd437f3392edd7
Leon Liang <leli@microsoft.com>
2018-12-22 18:14:24 -0800

    Fix backend

--- hello.go
+++ hello.go
@@ -10,10 +10,11 @@ import (
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/login", LoginHandler).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("TEST");
 	params := mux.Vars(r)
 	fmt.Print(params)
 	fmt.Println("_______________")
