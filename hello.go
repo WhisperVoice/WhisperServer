df95b142dce368866f263f3a52f9daa8c97d4ba7
Leon Liang <llian008@fiu.edu>
2019-02-10 16:33:04 -0800

    Add skeleton code

--- hello.go
+++ hello.go
@@ -9,14 +9,28 @@ import (
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/login", LoginHandler).Methods("POST")
+	router.HandleFunc("/login", loginHandler).Methods("POST")
+	router.HandleFunc("/signup", signupHandler).Methods("POST")
+	router.HandleFunc("/whispers", retrieveWhispersHandler).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func LoginHandler(w http.ResponseWriter, r *http.Request) {
+func loginHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("TEST");
 	params := mux.Vars(r)
 	fmt.Print(params)
 	fmt.Println("_______________")
 	fmt.Print(r.Body)
+}
+
+// TODO: Take sign up registration informationn and register to cosmosdb table https://docs.microsoft.com/en-us/rest/api/storageservices/table-service-rest-api
+func signupHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+// TODO: Retrieve voice clips for user post endpoint https://docs.microsoft.com/en-us/rest/api/storageservices/table-service-rest-api
+func retrieveWhispersHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+// TODO: Retrieve voice clips for user mainn function https://docs.microsoft.com/en-us/rest/api/storageservices/table-service-rest-api
+func retrieveWhispers(w http.ResponseWriter, r *http.Request) {
 }
